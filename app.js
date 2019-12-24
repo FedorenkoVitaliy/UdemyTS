695eb9c3d74fc60f9af6d975ff0834f39ebf4b6c
V.Fedorenko <v.fedorenko@cosmonova.net>
2019-12-24 15:39:22 +0200

    feat(types): tuple

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var person;
+var person = {
+    //const person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+person.role.push('admin');
 var favouritesActivities;
 favouritesActivities = ['Sports', 1,];
-for (var _i = 0, _a = personal.hobbies; _i < _a.length; _i++) {
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); // !!! ERROR !!!
 }
--- app.ts
+++ app.ts
@@ -1,16 +1,23 @@
 const person: {
-/*  name: string,
+  name: string,
   age: number,
-} = {*/
+  hobbies: string[],
+  role: [number, string], //tuple
+} = {
+//const person = {
   name: 'Maximilian',
   age: 30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+
+//person.role.push('admin'); error, but doesn't catch
+//person.role[1] = 10; //Error
 
 let favouritesActivities: any[];
 favouritesActivities = ['Sports', 1,];
 
- for(const hobby of personal.hobbies){
+ for(const hobby of person.hobbies){
    console.log(hobby.toUpperCase());
-   console.log(hobby.map()); // !!! ERROR !!!
+   //console.log(hobby.map()); // !!! ERROR !!!
  }
