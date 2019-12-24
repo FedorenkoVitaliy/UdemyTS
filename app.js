6618f4f1dddc80bcb679e3a14a43669278a5837e
V.Fedorenko <v.fedorenko@cosmonova.net>
2019-12-24 16:56:32 +0200

    feat(types): Literal types

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-function combine(input1, input2) {
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        return input1 + input2;
+function combine(input1, input2, resultConversion) {
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        return +input1 + +input2;
     }
     else {
         return input1.toString() + input2.toString();
     }
 }
-console.log('Combined Ages:', combine(30, 26));
-console.log('Combined Names:', combine('Max', 'Anna'));
+console.log('Combined Ages:', combine(30, 26, 'as-number'));
+console.log('Combined String Ages:', combine('30', '26', 'as-number'));
+console.log('Combined Names:', combine('Max', 'Anna', 'as-text'));
