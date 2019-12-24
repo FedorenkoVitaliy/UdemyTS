a19f93620bf4b23794a6b635e17cbb8fe794f03c
V.Fedorenko <v.fedorenko@cosmonova.net>
2019-12-24 18:35:06 +0200

    feat(functions): functions types

--- app.ts
+++ app.ts
@@ -7,3 +7,10 @@ function printResult(num:number): void {//not undefined, because undefined means
 }
 
 printResult(add(5,12));
+
+let combineValues: (a:number, b: number) => number; //Function;
+
+combineValues = add;
+//combineValues = printResult;
+
+console.log(combineValues(8, 8));
