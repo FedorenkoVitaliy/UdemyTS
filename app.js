3d4101e74c2a12c8ea39e14efb8b60bbbdce0f64
V.Fedorenko <v.fedorenko@cosmonova.net>
2019-12-24 14:44:24 +0200

    feat(types): string, number and boolean

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else
+        return n1 + n2;
 }
-var number1 = '5';
+var number1 = 5;
 var number2 = 2.6;
-console.log(add(number1, number2));
+var printResult = true;
+var resultPhrase = 'Result is:';
+add(number1, number2, printResult, resultPhrase);
